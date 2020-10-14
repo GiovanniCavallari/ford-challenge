f023668f71a1d8618f2ac48a342728c60dc62d82
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-10-14 10:24:37 -0300

    Add tire icon

--- mobile/src/assets/styles/icons.tsx
+++ mobile/src/assets/styles/icons.tsx
@@ -36,8 +36,8 @@ export const AlertsIcon: React.FC<Props> = ({ focused }) => (
 export const SensorsIcons: ISensorsIcons = {
   oil: <Fontisto name="blood-drop" size={24} color="#fff" />,
   fuel: <FontAwesome5 name="gas-pump" size={24} color="#fff" />,
-  tire: <MaterialIcons name="local-movies" size={24} color="#fff" />,
-  brake: <MaterialIcons name="layers" size={24} color="#fff" />,
-  odometer: <AntDesign name="dashboard" size={24} color="#fff" />,
+  tire: <MaterialCommunityIcons name="movie-roll" size={28} color="#fff" />,
+  brake: <MaterialCommunityIcons name="car-brake-abs" size={28} color="#fff" />,
+  odometer: <AntDesign name="dashboard" size={28} color="#fff" style={{ marginTop: -2 }} />,
   temperature: <FontAwesome5 name="temperature-low" size={24} color="#fff" />,
 };
--- mobile/src/screens/Car/index.tsx
+++ mobile/src/screens/Car/index.tsx
@@ -51,7 +51,7 @@ const Car: React.FC = () => {
   useFocusEffect(
     useCallback(() => {
       getCar();
-    }, [])
+    }, []),
   );
 
   const navigation = useNavigation();
@@ -83,18 +83,21 @@ const Car: React.FC = () => {
         </Header>
 
         <Main>
-          {car.sensors.map((sensor: ISensor) => (
-            <SensorInfo
-              key={sensor.name}
-              name={firstCapitalLetter(sensor.name)}
-              value={sensor.value}
-              unit={sensor.configurations.unit}
-              icon={SensorsIcons[sensor.name]}
-              onPress={() => handleCarToSensor(sensor.name)}
-              direction={sensor.configurations.direction}
-              configValue={sensor.configurations.value}
-            />
-          ))}
+          {car.sensors.map((sensor: ISensor) => {
+            const sensorName = firstCapitalLetter(sensor.name);
+            return (
+              <SensorInfo
+                key={sensor.name}
+                name={sensorName}
+                value={sensor.value}
+                unit={sensor.configurations.unit}
+                icon={sensorName.includes('tire') ? SensorsIcons['tire'] : SensorsIcons[sensor.name]}
+                onPress={() => handleCarToSensor(sensor.name)}
+                direction={sensor.configurations.direction}
+                configValue={sensor.configurations.value}
+              />
+            );
+          })}
         </Main>
       </Container>
     </Wrapper>
