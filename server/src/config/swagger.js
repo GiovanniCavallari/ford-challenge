5ee42b88ad284a6d8155ce972329780bea8a7e98
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-10-15 07:27:53 -0300

    Update env variables

--- server/src/config/swagger.js
+++ server/src/config/swagger.js
@@ -3,8 +3,8 @@ export default {
     info: {
       title: 'Ford AV API',
       description: 'API developed for the Ford Challenge in partnership with FIAP',
-      version: `${process.env.APPSETTING_API_VERSION}`,
-      servers: [`${process.env.APPSETTING_SERVER_URL}:${process.env.APPSETTING_SERVER_PORT}`],
+      version: `${process.env.API_VERSION}`,
+      servers: [`${process.env.SERVER_URL}:${process.env.SERVER_PORT}`],
       basePath: '/api',
     },
   },
--- server/src/server.js
+++ server/src/server.js
@@ -10,6 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.APPSETTING_SERVER_PORT, () => {
-  console.log(`Server running on ${process.env.APPSETTING_SERVER_URL}:${process.env.APPSETTING_SERVER_PORT}`);
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server running on ${process.env.SERVER_URL}:${process.env.SERVER_PORT}`);
 });
