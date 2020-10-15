8222cdbb101d04b2eff34b12863120577e81585f
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-10-15 17:47:09 -0300

    Update api url

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://c68a8617e0ed.ngrok.io',
+  baseURL: 'https://fordva-aylrs.ondigitalocean.app',
 });
 
 export default api;
