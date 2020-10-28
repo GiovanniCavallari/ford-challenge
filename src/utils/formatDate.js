8b4f655da43c86cda66940ee33d48af6bbf14923
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-10-28 12:21:19 -0300

    Fix locale on formatDate

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,7 +1,9 @@
 export const formatDateAndHour = (date) => {
-  const options = { hour: 'numeric', minute: 'numeric', timeZone: 'America/Sao_Paulo' };
-  const format = date.toLocaleDateString('pt-BR', options).split(' ');
-  const formatedDate = `${format[0]} - ${format[1]}`;
+  const options = { hour: 'numeric', minute: 'numeric', hour12: false, timeZone: 'America/Sao_Paulo' };
+  const format = date.toLocaleDateString('en-US', options).split(', ');
+
+  const dateString = format[0].split('/');
+  const formatedDate = `${dateString[1]}/${dateString[0]}/${dateString[2]} - ${format[1]}`;
 
   return formatedDate;
 };
