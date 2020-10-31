fc39ee12be8a8babb1f87c781dfa67fb9d70a422
Edilson <edilsonsts@users.noreply.github.com>
2020-10-31 00:16:39 -0300

    Remoção pyttsx3 e add nao()
    Removi a lib pyttsx3 e suas funções;
    Adicionei a função 'não' para quando respondermos não para a pergunta 'mais alguma coisa?' da assistente.

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -1,205 +1,141 @@
 import speech_recognition as sr
-import pyttsx3, requests
+import win32com.client as wincl
+import requests
 
 # Configurações
 r = sr.Recognizer()  # Cria uma nova instância de Recognizer, que representa uma coleção de configurações e funcionalidades de reconhecimento de fala
-engine = pyttsx3.init()  # inicia a engine da lib
-# caminho da voz para uma variavel
-# br_voz_id = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_PT-BR_MARIA_11.0'
-# muda a configura da voz para a da variavel acima
-# engine.setProperty('voice', br_voz_id)
 r.energy_threshold = 4000  # Representa o limite do nível de energia para sons. Valores abaixo desse limite são considerados silêncio e valores acima desse limite são considerados fala
-speech_rate = engine.getProperty('rate')  # pegando a propriedade 'rate'
-engine.setProperty('rate', speech_rate+63)  # aumenta em +65
-# Taxa de fala em palavras por minuto. O padrão é 200 palavras por minuto.
-
+windows_speak = wincl.Dispatch("SAPI.SpVoice")
+windows_speak.Rate = 3
 
 def interacao():
 
+    def nao():
+        speech = 0
+        escutar = 0
+        windows_speak.Speak('Ta bom, pode contar comigo sempre que precisar!')
+        return speech
+
     def combustivel(fala):
-        engine.say('Há ' + str(sensors('fuel')) + ' porcento do tanque de combustível. Deseja verificar mais alguma coisa?')
-        engine.runAndWait()
+        windows_speak.Speak('Há ' + str(sensors('fuel')) + ' porcento do tanque de combustível. Deseja verificar mais alguma coisa?')
         speech = 0
         audio = r.listen(s, 3, 7)
         speech = r.recognize_google(audio, language='pt')
         if 'não' in speech:
-            speech = 0
-            escutar = 0
-            engine.say('Ta bom, pode contar comigo sempre que precisar!')
-            engine.runAndWait()
-            return speech
+            nao()
 
     def oleoMotor(fala):
         if sensors('oil'):
-            engine.say('A pressão do óleo está baixa, favor verificar! Algo mais?')
-            engine.runAndWait()
+            windows_speak.Speak('A pressão do óleo está baixa, favor verificar! Algo mais?')
+           
         else:
-            engine.say('A pressão do óleo ok! Algo mais?')
-            engine.runAndWait()
+            windows_speak.Speak('A pressão do óleo ok! Algo mais?')
         speech = 0
         audio = r.listen(s, 3, 7)
         speech = r.recognize_google(audio, language='pt')
         if 'não' in speech:
-            speech = 0
-            escutar = 0
-            engine.say('Ta bom, pode contar comigo sempre que precisar')
-            engine.runAndWait()
-            return speech
+            nao()
 
     def freio(fala):
         if sensors('brake'):
-            engine.say('A pastilha de freio está desgastada, favor verificar! Algo mais')
-            engine.runAndWait()
+            windows_speak.Speak('A pastilha de freio está desgastada, favor verificar! Algo mais?')    
+            
         else:
-            engine.say('A pastilha de freio está ok! Algo mais?')
-            engine.runAndWait()
+            windows_speak.Speak('A pastilha de freio está ok! Algo mais?')
+            
         speech = 0
         audio = r.listen(s, 3, 7)
         speech = r.recognize_google(audio, language='pt')
         if 'não' in speech:
-            speech = 0
-            escutar = 0
-            engine.say('Ta bom, pode contar comigo sempre que precisar')
-            engine.runAndWait()
-            return speech
+           nao()
 
     def temperatura(fala):
-        engine.say('O motor esta à ' + str(sensors('temperature')) + ' graus celsius. Deseja verificar mais alguma coisa?')
-        engine.runAndWait()
+        windows_speak.Speak('O motor esta à ' + str(sensors('temperature')) + ' graus celsius. Deseja verificar mais alguma coisa?')
         speech = 0
         audio = r.listen(s, 3, 7)
         speech = r.recognize_google(audio, language='pt')
         if 'não' in speech:
-            speech = 0
-            escutar = 0
-            engine.say('Ta bom, pode contar comigo sempre que precisar!')
-            engine.runAndWait()
-            return speech
+            nao()
 
     def odometro(fala):
-        engine.say('O carro já rodou ' + str(sensors('odometer')) + ' quilometros. Deseja verificar mais alguma coisa?')
-        engine.runAndWait()
+        windows_speak.Speak('O carro já rodou ' + str(sensors('odometer')) + ' quilometros. Deseja verificar mais alguma coisa?')
         speech = 0
         audio = r.listen(s, 3, 7)
         speech = r.recognize_google(audio, language='pt')
         if 'não' in speech:
-            # continuar = 0
-            speech = 0
-            engine.say('Ta bom, pode contar comigo sempre que precisar!')
-            engine.runAndWait()
-            return speech
+            nao()
 
     def TempPneuDD(fala):#Dianteiro direito
 
-        engine.say('O pneu dianteiro direito está á ' + str(sensors('rfTireTemp')) + ' graus celsius. Deseja verificar mais alguma coisa?')
-        engine.runAndWait()
+        windows_speak.Speak('O pneu dianteiro direito está á ' + str(sensors('rfTireTemp')) + ' graus celsius. Deseja verificar mais alguma coisa?')
         speech = 0
         audio = r.listen(s, 3, 7)
         speech = r.recognize_google(audio, language='pt')
         if 'não' in speech:
-            # continuar = 0
-            speech = 0
-            engine.say('Ta bom, pode contar comigo sempre que precisar!')
-            engine.runAndWait()
-            return speech
+            nao()
 
     def TempPneuED(fala):#Dianteiro esquerdo
 
-        engine.say('O pneu dianteiro esquerdo está á ' + str(sensors('lfTireTemp')) + ' graus celsius. Deseja verificar mais alguma coisa?')
-        engine.runAndWait()
+        windows_speak.Speak('O pneu dianteiro esquerdo está á ' + str(sensors('lfTireTemp')) + ' graus celsius. Deseja verificar mais alguma coisa?')
         speech = 0
         audio = r.listen(s, 3, 7)
         speech = r.recognize_google(audio, language='pt')
         if 'não' in speech:
-            # continuar = 0
-            speech = 0
-            engine.say('Ta bom, pode contar comigo sempre que precisar!')
-            engine.runAndWait()
-            return speech
+            nao()
 
     def TempPneuDT(fala):#Traseiro direito
 
-        engine.say('O pneu traseiro direito está á ' + str(sensors('rrTireTemp')) + ' graus celsius. Deseja verificar mais alguma coisa?')
-        engine.runAndWait()
+        windows_speak.Speak('O pneu traseiro direito está á ' + str(sensors('rrTireTemp')) + ' graus celsius. Deseja verificar mais alguma coisa?')
         speech = 0
         audio = r.listen(s, 3, 7)
         speech = r.recognize_google(audio, language='pt')
         if 'não' in speech:
-            # continuar = 0
-            speech = 0
-            engine.say('Ta bom, pode contar comigo sempre que precisar!')
-            engine.runAndWait()
-            return speech
+            nao()
 
     def TempPneuET(fala):#Traseiro esquerdo
 
-        engine.say('O pneu traseiro esquerdo está á ' + str(sensors('rlTireTemp')) + ' graus celsius. Deseja verificar mais alguma coisa?')
-        engine.runAndWait()
+        windows_speak.Speak('O pneu traseiro esquerdo está á ' + str(sensors('rlTireTemp')) + ' graus celsius. Deseja verificar mais alguma coisa?')
         speech = 0
         audio = r.listen(s, 3, 7)
         speech = r.recognize_google(audio, language='pt')
         if 'não' in speech:
-            # continuar = 0
-            speech = 0
-            engine.say('Ta bom, pode contar comigo sempre que precisar!')
-            engine.runAndWait()
-            return speech
+            nao()
 
     def PressPneuDD(fala):#Dianteiro direito
 
-        engine.say('O pneu dianteiro direito tem ' + str(sensors('rfTirePressure')) + ' P S I. Deseja verificar mais alguma coisa?')
-        engine.runAndWait()
+        windows_speak.Speak('O pneu dianteiro direito tem ' + str(sensors('rfTirePressure')) + ' P S I. Deseja verificar mais alguma coisa?')
         speech = 0
         audio = r.listen(s, 3, 7)
         speech = r.recognize_google(audio, language='pt')
         if 'não' in speech:
-            # continuar = 0
-            speech = 0
-            engine.say('Ta bom, pode contar comigo sempre que precisar!')
-            engine.runAndWait()
-            return speech
+            nao()
 
     def PressPneuED(fala):#Dianteiro esquerdo
 
-        engine.say('O pneu dianteiro esquerdo tem ' + str(sensors('lfTirePressure')) + ' P S I. Deseja verificar mais alguma coisa?')
-        engine.runAndWait()
+        windows_speak.Speak('O pneu dianteiro esquerdo tem ' + str(sensors('lfTirePressure')) + ' P S I. Deseja verificar mais alguma coisa?')
         speech = 0
         audio = r.listen(s, 3, 7)
         speech = r.recognize_google(audio, language='pt')
         if 'não' in speech:
-            # continuar = 0
-            speech = 0
-            engine.say('Ta bom, pode contar comigo sempre que precisar!')
-            engine.runAndWait()
-            return speech
+            nao()
 
     def PressPneuDT(fala):#Traseiro direito
 
-        engine.say('O pneu traseiro direito tem ' + str(sensors('rrTirePressure')) + ' P S I. Deseja verificar mais alguma coisa?')
-        engine.runAndWait()
+        windows_speak.Speak('O pneu traseiro direito tem ' + str(sensors('rrTirePressure')) + ' P S I. Deseja verificar mais alguma coisa?')
         speech = 0
         audio = r.listen(s, 3, 7)
         speech = r.recognize_google(audio, language='pt')
         if 'não' in speech:
-            # continuar = 0
-            speech = 0
-            engine.say('Ta bom, pode contar comigo sempre que precisar!')
-            engine.runAndWait()
-            return speech
+            nao()
 
     def PressPneuET(fala):#Traseiro esquerdo
 
-        engine.say('O pneu traseiro esquerdo tem ' + str(sensors('rlTirePressure')) + ' P S I. Deseja verificar mais alguma coisa?')
-        engine.runAndWait()
+        windows_speak.Speak('O pneu traseiro esquerdo tem ' + str(sensors('rlTirePressure')) + ' P S I. Deseja verificar mais alguma coisa?')
         speech = 0
         audio = r.listen(s, 3, 7)
         speech = r.recognize_google(audio, language='pt')
         if 'não' in speech:
-            # continuar = 0
-            speech = 0
-            engine.say('Ta bom, pode contar comigo sempre que precisar!')
-            engine.runAndWait()
-            return speech
+            nao()
 
 
     def sensors(req):
@@ -219,10 +155,9 @@ def interacao():
 
                 if 'Ford' in speech:
                     escutar = 1
-                    engine.say(oi)
-                    engine.runAndWait()
-
-                elif escutar == 1:
+                    windows_speak.Speak(oi)
+                    
+                if escutar == 1:
                     if 'combustível' in speech or 'gasolina' in speech:
                         combustivel(speech)
 
@@ -262,8 +197,16 @@ def interacao():
                     elif 'pressão' in speech and 'pneu' in speech and 'esquerdo' in speech and 'traseiro' in speech:
                         PressPneuET(speech)
 
-                else:
-                    escutar = 0
+                    elif 'não' in speech or 'nada' in speech:
+                        escutar = 0
+                        windows_speak.Speak('Ok, precisando é só chamar!')
+                    
+                    elif speech == '': 
+                        escutar = 0
+                        windows_speak.Speak('Se precisar, é só chamar')
+
+                    else:
+                        escutar = 0
 
             except Exception:
                 pass
