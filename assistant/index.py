60c8bf9da4267baaf6054b1fc5d3296b9eb6946d
Vitor Jun Shiguematu <vitorjun20@gmail.com>
2020-10-31 16:27:13 -0300

    Update
    e.set

--- assistant/index.py
+++ assistant/index.py
@@ -8,5 +8,6 @@ e = threading.Event()
 threadConsumirFila = threading.Thread(name='fordav_interaction', target=interacao, args=(e,))
 threadConsumirFila.daemon = True
 threadConsumirFila.start()
+e.set()
 
 consumirFila(e)
