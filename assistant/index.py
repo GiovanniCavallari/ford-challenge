0affee61649dbfcf63c3fad0d4ab065b8667ef3a
Giovanni Cavallari <jobacavallari@hotmail.com>
2020-10-31 11:51:17 -0300

    Test threading.Event

--- assistant/index.py
+++ assistant/index.py
@@ -1,10 +1,39 @@
+# import threading
+
+# from assistant import interacao
+# from consumirFila import consumirFila
+
+# threadConsumirFila = threading.Thread(target=consumirFila)
+# threadConsumirFila.daemon = True
+# threadConsumirFila.start()
+
+# interacao()
+
 import threading
+import time
 
-from assistant import interacao
-from consumirFila import consumirFila
+# This function gets called by our thread.. so it basically becomes the thread innit..                    
+def wait_for_event(e):
+    while True:
+        print('\tTHREAD: This is the thread speaking, we are Waiting for event to start..')
+        event_is_set = e.wait()
+        print('\tTHREAD:  WHOOOOOO HOOOO WE GOT A SIGNAL  : %s', event_is_set)
+        e.clear()
 
-threadConsumirFila = threading.Thread(target=consumirFila)
-threadConsumirFila.daemon = True
-threadConsumirFila.start()
+# Main code.. 
+e = threading.Event()
 
-interacao()
+t = threading.Thread(name='your_mum', target=wait_for_event, args=(e,))
+t.start()
+
+while True:
+    print('MAIN LOOP: still in the main loop..')
+    time.sleep(4)
+    print('MAIN LOOP: I just set the flag..')
+    e.set()
+    print('MAIN LOOP: now Im gonna do some processing n shi-t')
+    time.sleep(4)
+    print('MAIN LOOP:  .. some more procesing im doing   yeahhhh')
+    time.sleep(4)
+    print('MAIN LOOP: ok ready, soon we will repeat the loop..')
+    time.sleep(2)
